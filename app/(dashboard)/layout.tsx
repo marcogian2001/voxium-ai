7b692250424c25b8360bffc099ca660718a413c6
Gianmarco <scandale.gianmarco+dev@gmail.com>
2025-12-24 19:15:01 +0100

    06: dashboard sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,27 @@
+import {
+  Sidebar,
+  SidebarHeader,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenu,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+import React, { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main className="flex flex-col h-screen w-screen bg-muted">
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default Layout;
