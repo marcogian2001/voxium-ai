c221edcd200ed58fbc98274b342c3915a1aea889
Gianmarco <scandale.gianmarco+dev@gmail.com>
2025-12-25 21:41:07 +0100

    09: Agents Setup

---
+++ modules/agents/ui/views/agents-view.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import ErrorState from "@/components/error-state";
+import LoadingState from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import React from "react";
+
+type Props = {};
+
+const AgentsView = (props: Props) => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+export default AgentsView;
+
+export const AgentsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a few seconds"
+    />
+  );
+};
+
+export const AgentsViewError = () => {
+  return (
+    <ErrorState
+      title="Error to load Agents"
+      description="Something went wrong"
+    />
+  );
+};
--- modules/home/ui/views/home-view.tsx
+++ modules/home/ui/views/home-view.tsx
@@ -7,8 +7,6 @@ import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
 
 export default function HomeView() {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "Antonio" }));
 
-  return <div>{data?.greeting}</div>;
+  return <div>Home View</div>;
 }
